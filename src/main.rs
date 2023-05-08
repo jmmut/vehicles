05e2e32088254f69e731b710bf1f1bb526857b6a
jose miguel mut <jomutlo@gmail.com>
2023-05-08 10:04:41 +0100

    hello macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+const DEFAULT_WINDOW_TITLE: &'static str = "Braitenberg Vehicles";
+const DEFAULT_WINDOW_WIDTH: i32 = 1280;
+const DEFAULT_WINDOW_HEIGHT: i32 = 720;
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        if is_key_down(KeyCode::Escape) {
+            break;
+        }
+        clear_background(SKYBLUE);
+        draw_rectangle(100.0, 200.0, 50.0, 150.0, DARKBLUE);
+        next_frame().await
+    }
+}
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: DEFAULT_WINDOW_TITLE.to_owned(),
+        window_width: DEFAULT_WINDOW_WIDTH,
+        window_height: DEFAULT_WINDOW_HEIGHT,
+        ..Default::default()
+    }
 }
