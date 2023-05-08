7f4dbbb1b039cabe0ee5d8c2e2a79bc0351481c9
jose miguel mut <jomutlo@gmail.com>
2023-05-08 12:37:02 +0100

    refactor draw_circle

--- src/draw.rs
+++ src/draw.rs
@@ -18,28 +18,18 @@ pub fn draw_vehicle(vehicle: &Vehicle) {
 pub fn draw_light(ligth: &Light) {
     // draw_circle_lines(ligth.position.x, ligth.position.y, ligth.radius, 2.0, DARKBROWN);
     // draw_circle_lines(ligth.position.x, ligth.position.y, ligth.radius, 2.0, YELLOW);
-    draw_circle(
-        ligth.position.x,
-        ligth.position.y,
-        ligth.radius * 0.25,
-        Color::new(0.99, 0.98, 0.00, 0.05),
-    );
-    draw_circle(
-        ligth.position.x,
-        ligth.position.y,
-        ligth.radius * 0.5,
-        Color::new(0.99, 0.98, 0.00, 0.05),
-    );
-    draw_circle(
-        ligth.position.x,
-        ligth.position.y,
-        ligth.radius * 0.75,
-        Color::new(0.99, 0.98, 0.00, 0.05),
-    );
-    draw_circle(
-        ligth.position.x,
-        ligth.position.y,
-        ligth.radius,
-        Color::new(0.99, 0.98, 0.00, 0.2),
-    )
+
+    let yellow_alpha = |alpha| Color::new(0.99, 0.98, 0.00, alpha);
+    let draw_circle_radius_alpha = |radius, alpha| {
+        draw_circle(
+            ligth.position.x,
+            ligth.position.y,
+            ligth.radius * radius,
+            yellow_alpha(alpha),
+        );
+    };
+    draw_circle_radius_alpha(0.25, 0.05);
+    draw_circle_radius_alpha(0.50, 0.05);
+    draw_circle_radius_alpha(0.75, 0.05);
+    draw_circle_radius_alpha(1.00, 0.2);
 }
